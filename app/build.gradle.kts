1064df7c516d9e12c66e862b88c0ac6fcaded387
sameerasw <sameerasw.com@gmail.com>
2026-08-28 10:24:28 +0530

    chore: drop min sdk level support to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sameerasw.medrop"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
